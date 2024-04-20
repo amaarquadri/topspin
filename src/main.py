475f9c13e91615560a7f5e8de85b227cf4397dee
thisisjaskaran <sodhi.jaskaransingh@gmail.com>
2024-04-20 01:19:51 -0400

    added main loop

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
+import time
 from frankapy import FrankaArm
+
 from config import Config
 import heuristics
 import planner
@@ -21,20 +23,33 @@ class MainNode:
 
         self.state = "waiting for prediction"
 
+        self.state_time = time.time()
+
     def pose_callback(self, msg):
         self.ball_pose = msg
 
     def update(self):
-
-        if heuristics.time_to_contact(x, v) < self.config.delta_t:
-            self.state = "waiting to swing"
-
         x = [self.ball_pose.pose.position.x, self.ball_pose.pose.position.y, self.ball_pose.pose.position.z]
         v = [self.ball_pose.pose.orientation.x, self.ball_pose.pose.orientation.y, self.ball_pose.pose.orientation.z]
 
-        t, q, q_pre, e_dot_des = planner.get_plan(x, v, self.franka_arm)
-        
+        q = None
+        q_pre = None
 
+        if (self.state == "waiting for prediction") and (time.time() - self.state_time < 0.2):
+            return
+
+        elif heuristics.time_to_contact(x, v) > self.config.delta_t and self.state == "waiting for prediction":
+            self.state = "waiting to swing"
+            t, q, q_pre, e_dot_des = planner.get_plan(x, v, self.franka_arm)
+            self.franka_arm.goto_joints(q_pre)
+        
+        elif heuristics.time_to_contact(x, v) < self.config.delta_t:
+            self.state = "waiting for prediction"
+            self.franka_arm.goto_joints(q + (q - q_pre))
+
+            self.state_time = time.time()
+            self.franka_arm.goto_joints(q_pre)
+        
     @staticmethod
     def run():
         rospy.spin()

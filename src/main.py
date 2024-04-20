e2d6b9155b41d7b947c3a789695f031ab2c52a85
Amaar Quadri <amaarquadri@gmail.com>
2024-04-19 22:23:05 -0400

    create main node

--- src/main.py
+++ src/main.py
@@ -1,5 +1,34 @@
+import rospy
+from geometry_msgs.msg import PoseStamped
+from frankapy import FrankaArm
+
+
+class MainNode:
+    def __init__(self):
+        rospy.init_node('ball_tracking_ekf', anonymous=True)
+        self.subscriber = rospy.Subscriber('/ball_pose', PoseStamped, self.pose_callback)
+        self.ball_pose = None
+
+        self.franka_arm = FrankaArm(with_gripper=False)
+        self.franka_arm.stop_skill()
+        self.franka_arm.reset_joints()
+
+        rospy.Timer(rospy.Duration(0.01), self.update)
+
+    def pose_callback(self, msg):
+        self.ball_pose = msg
+
+    def update(self):
+        pass
+
+    @staticmethod
+    def run():
+        rospy.spin()
+
+
 def main():
-    pass
+    main_node = MainNode()
+    main_node.run()
 
 
 if __name__ == '__main__':
